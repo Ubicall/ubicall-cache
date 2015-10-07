8d571da4e9938c0483a21dfe366cf7726937bfd2
waleed samy <waleed_samy@ymail.com>
2015-10-07 16:44:03 +0200

    add dummy cache code enbale jshint check before commit

--- settings.js
+++ settings.js
@@ -4,18 +4,18 @@ var log = require('./log');
 process.env.node_env = process.env.node_env || 'development';
 process.env.config_version = process.env.config_version || 20150920;
 
-var DEVENV = (process.env.node_env == "development" || process.env.node_env == "test");
+var DEVENV = (process.env.node_env === "development" || process.env.node_env === "test");
 if (DEVENV) {
   process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
 }
 
 if (!process.env.config_version) {
-  log.help("You should specify configuration version to use, see README for detail")
+  log.help("You should specify configuration version to use, see README for detail");
   throw new Error("Missed config_version environment variable");
 }
 
 var config = loader.load({
-  configHost: process.env.node_env == "production" ? "http://developer.ubicall.com/conf/" : "http://developer.dev.ubicall.com/conf/",
+  configHost: process.env.node_env === "production" ? "http://developer.ubicall.com/conf/" : "http://developer.dev.ubicall.com/conf/",
   configVersion: process.env.config_version,
   configEnv: process.env.node_env
 });
@@ -33,4 +33,4 @@ module.exports = {
     cacheModule: 'redis',
     redis: config.cache.redis
   }
-}
+};
